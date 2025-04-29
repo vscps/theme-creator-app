0d39a7850105c9e00a9d9283e39f2f2948fa5a36
Daniel <daniel.seebacher17@gmail.com>
2025-04-29 15:56:37 +0200

    Added the delete, cancel and confirmation message elements

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,10 @@
 import "./Color.css";
+import Button from "../Button/Button";
+import { useState } from "react";
 
 export default function Color({ color }) {
+  const [isVisible, setVisibility] = useState(false);
+  console.log(isVisible);
   return (
     <div
       className="color-card"
@@ -12,6 +16,22 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      <div className="deletion-area">
+        <div className={`message ${isVisible ? "" : "hidden"}`}>
+          Really delete?
+        </div>
+        <Button
+          type="button"
+          text="Cancel"
+          className={isVisible ? "" : "hidden"}
+          onClick={() => setVisibility(!isVisible)}
+        ></Button>
+        <Button
+          type="button"
+          text="Delete"
+          onClick={() => setVisibility(!isVisible)}
+        ></Button>
+      </div>
     </div>
   );
 }
